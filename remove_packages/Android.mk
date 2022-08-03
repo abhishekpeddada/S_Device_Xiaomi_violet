b81188f693c3b8b8a2b2812f51883d1f35896c93
rdx420 <padraramesh420@gmail.com>
2022-08-03 14:09:24 +0530

    violet: Update Some Apps on Remove_Packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,11 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail Drive ConnMO DCMO YouTube RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt PrebuiltGmail Drive ConnMO DCMO YouTube RecorderPrebuilt CalculatorGooglePrebuilt CalendarGooglePrebuilt PrebuiltDeskClockGoogle
 LOCAL_OVERRIDES_PACKAGES += DMService GCS GoogleCamera MaestroPrebuilt Maps MicropaperPrebuilt MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += Photos PrebuiltBugle SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap talkback PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase Snap talkback PixelLiveWallpaperPrebuilt
 LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt SprintDM SprintHM TurboPrebuilt Tycho USCCDM VZWAPNLib AndroidAutoStubPrebuilt
-LOCAL_OVERRIDES_PACKAGES += PixelWallpapers2020  VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt 
+LOCAL_OVERRIDES_PACKAGES += PixelWallpapers2021 VzwOmaTrigger OBDM_Permissions obdm_stub arcore DevicePolicyPrebuilt 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
