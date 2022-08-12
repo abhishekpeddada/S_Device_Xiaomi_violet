51117aabb34db7d2d192552c16a503fc45de7264
rdx420 <padraramesh420@gmail.com>
2022-08-12 13:43:15 +0530

    violet: Track Kernel from my github

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/rdx420/S_vendor_xiaomi_violet.git -b blaze vendor/xiaomi/violet
-git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b twelve kernel/xiaomi/violet
+git clone https://github.com/rdx420/kernel_xiaomi_violet_A12.git -b twelve kernel/xiaomi/violet
 git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
 git clone https://gitlab.com/Joker-V2/android_vendor_ANXCamera.git -b twelve vendor/ANXCamera
 rm -rf packages/resources/devicesettings
