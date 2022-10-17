196f1694c82002e7b71bba148c35548c49d4562c
abhishekpeddada <59862552+abhishekpeddada@users.noreply.github.com>
2022-10-17 12:51:06 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_violet.mk
+    $(LOCAL_DIR)/arrow_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_violet-userdebug \
-    lineage_violet-user \
-    lineage_violet-eng
+    arrow_violet-userdebug \
+    arrow_violet-user \
+    arrow_violet-eng
