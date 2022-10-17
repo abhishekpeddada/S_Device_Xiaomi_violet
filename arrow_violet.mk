3d7f4019d5cdea3649d400ead6e92139f1b19886
abhishekpeddada <59862552+abhishekpeddada@users.noreply.github.com>
2022-10-17 12:50:16 +0530

    Update and rename lineage_violet.mk to arrow_violet.mk

--- arrow_violet.mk
+++ arrow_violet.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -23,11 +23,11 @@ $(call inherit-product, vendor/ANXCamera/config.mk)
 WITH_GAPPS := true
 
 #Blaze
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := 尺りﾒ55 
+ARROW_BUILD_TYPE := UNOFFICIAL
+ARROW_MAINTAINER := abhix202
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := arrow_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
