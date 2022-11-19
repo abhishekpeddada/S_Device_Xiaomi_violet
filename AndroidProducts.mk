d175b02e17177142cdfdc5c1f18155574dcc851d
abhishekpeddada <59862552+abhishekpeddada@users.noreply.github.com>
2022-11-19 19:28:16 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_violet.mk
+    $(LOCAL_DIR)/cipher_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_violet-userdebug \
-    dot_violet-user \
-    dot_violet-eng
+    cipher_violet-userdebug \
+    cipher_violet-user \
+    cipher_violet-eng
